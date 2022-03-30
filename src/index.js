650e9b1fe831820014bc161f0a7ea1070f99e401
Mahammedi Abdelghani <wboductqr@relay.firefox.com>
2022-03-30 16:37:47 +0100

    fix: remove fontawesome & add: sass  process
    replaced by svg icons

--- src/index.js
+++ src/index.js
@@ -4,13 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
-
-library.add(fab, faCheckSquare, faCoffee)
-
-
 
 ReactDOM.render(
   <React.StrictMode>
